fe51ad826ce5e42e63d7ccebfbb473551724ff2e
pipi <469250376@qq.com>
2020-02-15 17:47:23 +0800

    Refactor the code structure

---
+++ src/main/java/com/pp/app/MyBatisTest.java
@@ -0,0 +1,15 @@
+package com.pp.app;
+
+import org.junit.Test;
+
+/**
+ * @author pipi
+ * @date 2020-02-15 17:46:29
+ */
+public class MyBatisTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Hello");
+    }
+}
